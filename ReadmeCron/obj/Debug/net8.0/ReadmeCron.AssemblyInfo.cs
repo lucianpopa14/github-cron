1dfe218f844b90321d008ff55ae8a81fa3998f4f
lucian popa <lucianpopa98@yahoo.com>
2025-09-17 19:19:53 +0300

    yml file had the wrong spaces

--- ReadmeCron/obj/Debug/net8.0/ReadmeCron.AssemblyInfo.cs
+++ ReadmeCron/obj/Debug/net8.0/ReadmeCron.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReadmeCron")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af7b5e122beacf4a2b3c44a890ec02fcedbc67d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ab14c4dae4366bb888dbe72322eca3ff7d317c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReadmeCron")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReadmeCron")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
